b46aa4a8a170f1db053e1b2e98e592ed00c9e6b6
guptaapoorv2005-sudo <guptaapoorv2005@gmail.com>
2026-01-10 01:55:44 +0530

    Add like routes for videos, comments, and tweets with JWT verification

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import healthcheckRouter from "./routes/healthcheck.router.js"
 import tweetRouter from "./routes/tweet.router.js"
 import subscriptionRouter from "./routes/subscription.route.js"
 import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
 
 //routes declare
 app.use("/api/v1/users",userRouter)
@@ -33,5 +34,6 @@ app.use("/api/v1/healthcheck",healthcheckRouter)
 app.use("/api/v1/tweets",tweetRouter)
 app.use("/api/v1/subscriptions",subscriptionRouter)
 app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/likes",likeRouter)
 
 export { app }
---
+++ src/routes/like.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
+
+const router = Router()
+
+router.use(verifyJWT)
+
+router.route("/video/:videoId").post(toggleVideoLike)
+
+router.route("/comment/:commentId").post(toggleCommentLike)
+
+router.route("/tweet/:tweetId").post(toggleTweetLike)
+
+router.route("/").get(getLikedVideos)
+
+export default router
